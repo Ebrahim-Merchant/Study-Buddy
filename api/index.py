7d11a44bc14a6535db21ba87a48fd7aab9ada335
Ebrahim Merchant <ebrahim.merchant@uoit.net>
2019-04-02 23:25:10 -0400

    Add venv for python

--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
 import pyrebase
+from flask import Flask, current_app, send_from_directory
+from flask_socketio import SocketIO
+import os
+import sys
 
 config = {
     "apiKey": "AIzaSyDCb-dS1ulUx1jiatzyKViayr5uVe1EAfU",
@@ -9,8 +13,28 @@ config = {
     "messagingSenderId": "672869013696"
 }
 
+app = Flask(__name__, static_folder=os.getcwd(), static_url_path='')
+app.config['SECRET_KEY'] = config["apiKey"]
+socketio = SocketIO(app)
+
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
+@app.route("/")
+def hello():
+
+    print(app.static_url_path + '\\index.html'+ " "+app.static_folder, file=sys.stdout)
+    return current_app.send_static_file('index.html')
+
+@app.route("/login")
 def login(email, password):
     auth.sign_in_with_email_and_password(email, password)
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
